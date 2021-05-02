f450afffda0e014e868a8c65ab8fdc0560b39d93
harshit <patelharshit2013@gmai.com>
2021-05-01 21:24:45 -0400

    added login

--- webapp/urls.py
+++ webapp/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path(r'',views.index,name='index'),
     path(r'user_blog/',views.userBlog,name='user_blog'),
     path(r'register/',views.register,name='register'),
+    path(r'login/',views.login,name='login'),
+    path(r'logout/',views.logout,name='logout'),
+    path(r'update_blog/<int:id>/',views.updateBlog,name='update_blog'),
     path(r'update_blog/<int:id>/',views.updateBlog,name='update_blog'),
     path(r'delete_blog/<int:id>/',views.deleteBlog,name='delete_blog'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 import requests
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def index(request):
@@ -20,4 +24,36 @@ def deleteBlog(request,id):
     return render(request,'index.html')
 
 def register(request):
+    c={}
+    if request.method == "POST":
+        fname=request.POST.get('firstName')
+        lname=request.POST.get('lastName')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        if not User.objects.filter(email=email).exists():
+            user_details=User(first_name=fname,last_name=lname,email=email,username=email)    
+            user_details.set_password(password)
+            user_details.save()
+            return render(request,'index.html')
+        else:
+            c={'error':'Email Exist','fname':fname,'lname':lname,'email':email,'password':password}
+            return render(request,'register.html',c)
     return render(request,'register.html')
+
+def login(request):
+    c={}
+    if request.method == "POST":
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        user = authenticate(username=email, password=password)
+        if user and user.is_active:
+            auth_login(request, user)
+            print(user.username)
+            return redirect('webapp:index')
+        else:
+            c={'error':'Invalid Email or Password. Please try again.'}
+    return render(request,'login.html',c)
+
+def logout(request):
+    auth_logout(request)
+    return index(request)
